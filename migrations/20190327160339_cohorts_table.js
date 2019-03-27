ff8b710eb9bc1271aac1791f9f40837a1f086ee3
Jamie Goodnight <goodnightlambda@gmail.com>
2019-03-27 16:23:02 -0400

    Forgot to code the migration functions. Working now. Returning empty cohort array.

--- migrations/20190327160339_cohorts_table.js
+++ migrations/20190327160339_cohorts_table.js
@@ -1,3 +1,14 @@
-exports.up = function(knex, Promise) {};
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("cohorts", function(tbl) {
+    //   primary key called id make it auto-increment
+    tbl.increments();
+    tbl
+      .string("name", 128)
+      .notNullable()
+      .unique();
+  });
+};
 
-exports.down = function(knex, Promise) {};
+exports.down = function(knex, Promise) {
+  return knex.scheme.dropTableIfExists("cohorts");
+};
